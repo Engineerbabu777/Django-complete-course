d4a6976e24da7b53267cd880a727bceb2c5b8131
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-25 09:28:30 +0500

    add: learned to attach

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,8 +1,16 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
+# def homepage(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+# def about(request):
+#     return HttpResponse("This is the about page")
+    
+
 
 def homepage(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request,'home.html')
 
 def about(request):
-    return HttpResponse("This is the about page")
+     return render(request,'about.html')
     

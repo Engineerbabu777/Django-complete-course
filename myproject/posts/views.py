199533594b0fad15d07336c8f2f306363cc0bf9a
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-25 13:14:46 +0500

    Add: learned about templates and extending and blocks adding, migrations and models creations, creating model method, understanding admin, getting data from db, passing to templates!

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def posts_lists(request):
-    return render(request, 'posts/post_list.html')
+    post = Post.objects.all().order_by('-date')
+    return render(request, 'posts/post_list.html', {'posts':post})
+
+
